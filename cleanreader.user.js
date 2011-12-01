858f0aff9a01ff108bcb266bb426948b313159c8
Aki Saarinen <aki@akisaarinen.fi>
2011-12-01 11:11:10 +0200

    Fix to work with new Reader style
    A new logo section appeared, hide it. Also fix the height of the button
    box on the left, buttons were wrongly aligned with the new look.

--- cleanreader.user.js
+++ cleanreader.user.js
@@ -1,5 +1,5 @@
 function addButton() {
-    $("#lhn-add-subscription-section").append('<div id="toggle-search-button" role="button" class="goog-inline-block jfk-button jfk-button-action" tabindex="1" style="-webkit-user-select: none; " title="Search"><img src="//ssl.gstatic.com/ui/v1/button/search-white.png" style="width: 21px; height: 21px; " class="jfk-button-img"></div>');
+    $("#lhn-add-subscription-section").append('<span id="toggle-search-button" role="button" class="goog-inline-block jfk-button jfk-button-action" tabindex="1" style="-webkit-user-select: none; " title="Search"><img src="//ssl.gstatic.com/ui/v1/button/search-white.png" style="width: 21px; height: 21px; " class="jfk-button-img"></span>');
     $("#toggle-search-button").click(function() {
       $("#top-bar").toggle();
       $("#gb").toggle();
@@ -7,9 +7,10 @@ function addButton() {
 }
 function addStyles() {
     var styles =
-        "#top-bar, #gb { display: none; } " + 
+        "#top-bar, #gb, #logo-section { display: none; } " +
         "#viewer-header, #lhn-add-subscription-section, #sections-header { height: 35px; } " +
         "#home-section { padding: 0; } " + 
+        "#lhn-add-subscription { margin-top: -31px; } " +
         ".lhn-section-primary { line-height: 21px; } " +
         "#reading-list-unread-count { margin-top: 0; } " + 
         "#viewer-entries-container { height: 100%; } " + 
@@ -20,3 +21,6 @@ function addStyles() {
 }
 addButton();
 addStyles();
+
+// Need to do it this way or Reader will override
+$("#lhn-add-subscription-section").css("height", "35px");

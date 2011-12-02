f7fef1ae16457b93b4552a05439b65e238b70546
Aki Saarinen <aki@akisaarinen.fi>
2011-12-02 18:39:39 +0200

    Fix search bar for old version and bump version to 0.2.3

--- cleanreader.user.js
+++ cleanreader.user.js
@@ -2,6 +2,10 @@ function addButton() {
     $("#lhn-add-subscription-section").append('<span id="toggle-search-button" role="button" class="goog-inline-block jfk-button jfk-button-action" tabindex="1" style="-webkit-user-select: none; " title="Search"><img src="//ssl.gstatic.com/ui/v1/button/search-white.png" style="width: 21px; height: 21px; " class="jfk-button-img"></span>');
     $("#toggle-search-button").click(function() {
       $("#gb").toggle();
+      // Show the top-bar for old layout only
+      if (!document.getElementById("logo-section")) {
+        $("#top-bar").toggle();
+      }
     });
 }
 function addStyles() {
@@ -14,9 +18,8 @@ function addStyles() {
         "#viewer-entries-container { height: 100%; } " + 
         "#scrollable-sections { height: 100%; } ";
 
-    // Work with both new and old styles
-    var hasLogoSection = $("#logo-section").length == 1;
-    if (hasLogoSection) {
+    // Fix for new layout
+    if (document.getElementById("logo-section")) {
         styles += "#lhn-add-subscription { margin-top: -31px; } ";
     }
 
